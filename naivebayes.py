06c7700c7a9acdd23126c87a826fe025a2850c56
derek <derekli1995@outlook.com>
2018-03-14 19:33:36 -0400

    before 大改

--- naivebayes.py
+++ naivebayes.py
@@ -3,6 +3,7 @@ import numpy as np
 import random
 
 set_order = {'training': 0, 'validating': 1, 'testing': 2, 'all': 3}
+word_pos_neg = {'real': 1, 'fake': 0}
 
 
 def get_lines():
@@ -25,7 +26,7 @@ def get_words(sets):
     for line in sets:
         for word in line.strip().split():
             if word not in all_words:
-                all_words[word] = {'real': 0.0, 'fake': 0.0}
+                all_words[word] = [0.0, 0.0]
     return all_words
 
 
@@ -34,9 +35,9 @@ def words_count(sets, expect):
     for i in range(len(sets)):
         for word in sets[i].strip().split():
             if expect[i] == 1:
-                all_words[word]['real'] += 1.0
+                all_words[word][1] += 1.0
             else:
-                all_words[word]['fake'] += 1.0
+                all_words[word][0] += 1.0
     return all_words
 
 
@@ -88,25 +89,25 @@ def top_words(sets, expect):
     all_words = words_count(sets, expect)
     top = {}
     for word, c in all_words.items():
-        top[word] = c['real'] + c['fake']
+        top[word] = c[1] + c[0]
     words = sorted(top, key=top.get, reverse=True)[:3]
     return words, all_words
 
 
-def log_prob_words_given_C(line, words_count, pos_count, neg_count, m, k, cls):
+def log_prob_words_given_C(line, words_count, pos_count, neg_count, m, p, cls):
     sumlog = 0
     word_list = line.strip().split()
     for word in word_list:
         if cls == 1:
-            prob_word = np.log((words_count[word]['real'] + m * k) / (pos_count + k))
+            prob_word = np.log((words_count[word][cls] + m * p) / (pos_count + p))
         else:
-            prob_word = np.log((words_count[word]['fake'] + m * k) / (neg_count + k))
+            prob_word = np.log((words_count[word][cls] + m * p) / (neg_count + p))
         sumlog += prob_word
     return sumlog
 
 
-def log_prob_C_given_words(line, words_count, pos_count, neg_count, m, k, cls):
-    log_prob_words_cls = log_prob_words_given_C(line, words_count, pos_count, neg_count, m, k, cls)
+def log_prob_C_given_words(line, words_count, pos_count, neg_count, m, p, cls):
+    log_prob_words_cls = log_prob_words_given_C(line, words_count, pos_count, neg_count, m, p, cls)
     if cls == 1:
         prob_C = pos_count / (pos_count + neg_count)
     else:
@@ -114,27 +115,30 @@ def log_prob_C_given_words(line, words_count, pos_count, neg_count, m, k, cls):
     return log_prob_words_cls + np.log(prob_C)
 
 
-def predict(line, words_count, pos_count, neg_count, m, k):
-    prob_pos = log_prob_C_given_words(line, words_count, pos_count, neg_count, m, k, 1)
-    prob_neg = log_prob_C_given_words(line, words_count, pos_count, neg_count, m, k, 0)
+def predict(line, words_count, pos_count, neg_count, m, p):
+    prob_pos = log_prob_C_given_words(line, words_count, pos_count, neg_count, m, p, 1)
+    prob_neg = log_prob_C_given_words(line, words_count, pos_count, neg_count, m, p, 0)
     if prob_pos >= prob_neg:
         return 1
     else:
         return 0
 
 
-def get_performance(sets, expect, m, k):
-    words_c = words_count(sets,expect)
+def get_performance(sets, expect, m, p,validating,validating_expect):
+    words_c = words_count(validating, validating_expect)
     results = []
     for line in sets:
-        results.append(predict(line, words_c, expect.count(1), expect.count(0), m, k))
+        results.append(predict(line, words_c, expect.count(1), expect.count(0), m, p))
     return np.sum(np.array(results) == np.array(expect)) / (len(expect) * 1.0)
 
 
 if __name__ == '__main__':
     training, validating, testing, allset, expected = generate_sets()
-    topwords, all_words = top_words(allset,expected[3])
+    topwords, all_words = top_words(allset, expected[3])
     print(topwords)
-    print(get_performance(validating,expected[1],1.0,0.1))
-    print(get_performance(testing, expected[2], 1.0, 0.1))
-    print(get_performance(training, expected[0], 1.0, 0.1))
+    m = 1.0
+    p = 0.05
+    print(get_performance(validating, expected[1], m, p,validating, expected[1]))
+    print(get_performance(testing, expected[2], m, p,validating, expected[1]))
+    # print(get_performance(training, expected[0], m, p))
+    # print(get_performance(allset, expected[3], m, p))

ca9efa16943a0d0bda6bb1b03ef561d98a981c90
derek <derekli1995@outlook.com>
2018-03-16 19:00:16 -0400

    part4,6 finished: learning curve top thetas

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MLa3
+part 6ab:
+For real news:
+top 10 positive theta: ['trumps', 'turnbull', 'us', 'trump', 'says', 'donald', 'korea', 'debate', 'north', 'ban']
+top 10 negative theta: ['hillary', 'watch', 'breaking', 'just', 'america', 'victory', 'new', 'are', 'the', 'they']
+For fake news:
+top 10 positive theta: ['trumps', 'turnbull', 'us', 'trump', 'says', 'donald', 'korea', 'debate', 'north', 'ban']
+top 10 negative theta: ['hillary', 'watch', 'breaking', 'just', 'america', 'victory', 'new', 'are', 'the', 'they']
+~~~~~~~~~~~~~~~After pruning stopwords~~~~~~~~~~~~~~~
+For real news:
+top 10 positive theta: ['trumps', 'turnbull', 'trump', 'says', 'donald', 'korea', 'debate', 'north', 'ban', 'comey']
+top 10 negative theta: ['hillary', 'watch', 'breaking', 'just', 'america', 'victory', 'new', 'voter', 'star', 'supporter']
+For fake news:
+top 10 positive theta: ['trumps', 'turnbull', 'trump', 'says', 'donald', 'korea', 'debate', 'north', 'ban', 'comey']
+top 10 negative theta: ['hillary', 'watch', 'breaking', 'just', 'america', 'victory', 'new', 'voter', 'star', 'supporter']
--- logistic.py
+++ logistic.py
@@ -2,6 +2,8 @@ from naivebayes import generate_sets
 from torch.autograd import Variable
 import torch
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 
 label = {'real': [1, 0], 'fake': [0, 1]}
 set_order = {'training': 0, 'validating': 1, 'testing': 2, 'all': 3}
@@ -40,6 +42,7 @@ def generate_x_y(setnum):
     words_dict = all_words.copy()
     x = np.zeros((len(all_words), len(sets)))
     i = 0
+    words_order = [w for w in all_words.keys()]
     for line in sets:
         for word in line.strip().split():
             if word in words_dict:
@@ -50,7 +53,7 @@ def generate_x_y(setnum):
     y = np.array(expected[setnum])
     temp_y = 1 - y
     y = np.vstack((y, temp_y))
-    return x, y
+    return x, y, words_order
 
 
 def build_model(input_dim, output_dim):
@@ -83,7 +86,7 @@ def train_l2(model, x, y, n):
             # forward
             y_pred = model(mini_x)
             loss = loss_fn(y_pred, mini_y_labels)
-            # add l1 regularization
+            # add l2 regularization
             reg_lambda = 1.0 / x.shape[1]
             l2_reg = None
             for W in model.parameters():
@@ -150,15 +153,86 @@ def predict(model, x, y):
     return performance
 
 
+def theta_dict(model, words_order):
+    positve_thetas = {}
+    negative_thetas = {}
+    thetas = np.array([t.data.numpy() for t in model.parameters()])[0]
+    thetas = thetas.T
+    i = 0
+    for w in words_order:
+        positve_thetas[w] = thetas[i, 0]
+        negative_thetas[w] = thetas[i, 1]
+        i += 1
+    return positve_thetas, negative_thetas
+
+
 if __name__ == '__main__':
-    x, y = generate_x_y(0)
+    print("==================part1====================")
+    x, y, words_order = generate_x_y(0)
+    model_l2 = build_model(x.shape[0], y.shape[0])
+    train = []
+    validate = []
+    test = []
+    for i in range(0, 500, 20):
+        model_l2 = train_l2(model_l2, x, y, i)
+        Y_l2 = predict(model_l2, x, y)
+        v_x, v_y, _ = generate_x_y(1)
+        v_Y_l2 = predict(model_l2, v_x, v_y)
+        t_x, t_y, _ = generate_x_y(2)
+        t_Y_l2 = predict(model_l2, t_x, t_y)
+        train.append(Y_l2)
+        validate.append(v_Y_l2)
+        test.append(t_Y_l2)
+        model_l2 = build_model(x.shape[0], y.shape[0])
+
+    plt.step(range(0, 500, 20), train, label='Training set')
+    plt.step(range(0, 500, 20), validate, label='Validating set')
+    plt.step(range(0, 500, 20), test, label='Testing set')
+    plt.title('Part 4')
+    plt.xlabel('epochs')
+    plt.ylabel("Performance")
+    plt.legend()
+    plt.savefig("part4.jpg")
+    plt.gca().clear()
+    print('learning curve saved')
+
+    print("==================part2====================")
     model_l2 = build_model(x.shape[0], y.shape[0])
     model_l1 = build_model(x.shape[0], y.shape[0])
     model_l2 = train_l2(model_l2, x, y, 200)
     model_l1 = train_l1(model_l1, x, y, 200)
-    x, y = generate_x_y(2)
+    x, y, _ = generate_x_y(2)
     predY_l2 = predict(model_l2, x, y)
     predY_l1 = predict(model_l1, x, y)
     print('============Trained using training set, test using testing set============')
-    print('L2 Regularization accuracy:',predY_l2)
-    print('L2 Regularization accuracy:',predY_l1)
+    print('L2 Regularization accuracy:', predY_l2)
+    print('L1 Regularization accuracy:', predY_l1)
+
+    print("==================part3====================")
+    positve_thetas, negative_thetas = theta_dict(model_l2, words_order)
+    print('For real news:')
+    r_strong = sorted(positve_thetas, key=positve_thetas.get, reverse=True)[:10]
+    r_weak = sorted(positve_thetas, key=positve_thetas.get, reverse=False)[:10]
+    print("top 10 positive theta:", r_strong)
+    print("top 10 negative theta:", r_weak)
+    print('For fake news:')
+    f_strong = sorted(negative_thetas, key=negative_thetas.get, reverse=True)[:10]
+    f_weak = sorted(negative_thetas, key=negative_thetas.get, reverse=False)[:10]
+    print("top 10 positive theta:", r_strong)
+    print("top 10 negative theta:", r_weak)
+    for word in ENGLISH_STOP_WORDS:
+        if word in positve_thetas.keys():
+            positve_thetas.pop(word)
+        if word in negative_thetas.keys():
+            negative_thetas.pop(word)
+    print("~~~~~~~~~~~~~~~After pruning stopwords~~~~~~~~~~~~~~~")
+    print('For real news:')
+    r_strong = sorted(positve_thetas, key=positve_thetas.get, reverse=True)[:10]
+    r_weak = sorted(positve_thetas, key=positve_thetas.get, reverse=False)[:10]
+    print("top 10 positive theta:", r_strong)
+    print("top 10 negative theta:", r_weak)
+    print('For fake news:')
+    f_strong = sorted(negative_thetas, key=negative_thetas.get, reverse=True)[:10]
+    f_weak = sorted(negative_thetas, key=negative_thetas.get, reverse=False)[:10]
+    print("top 10 positive theta:", r_strong)
+    print("top 10 negative theta:", r_weak)
